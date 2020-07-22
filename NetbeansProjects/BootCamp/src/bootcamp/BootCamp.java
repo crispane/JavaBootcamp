3a59cf6928346c0a848c7683ba752e28f8eb5e0a
crispane <crispane@pm.me>
2020-07-23 01:21:42 +0300

    add BootCamp date project

---
+++ NetbeansProjects/BootCamp/src/bootcamp/BootCamp.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bootcamp;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author f3nix
+ */
+public class BootCamp {
+		private String name;
+		private LocalDate startingDate;
+		private LocalDate endingDate;
+
+	public BootCamp(String name, LocalDate startingDate, LocalDate endingDate) {
+		this.name = name;
+		this.startingDate = startingDate;
+		this.endingDate = endingDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getStartingDate() {
+		return startingDate;
+	}
+
+	public void setStartingDate(LocalDate startingDate) {
+		this.startingDate = startingDate;
+	}
+
+	public LocalDate getEndingDate() {
+		return endingDate;
+	}
+
+	public void setEndingDate(LocalDate endingDate) {
+		this.endingDate = endingDate;
+	}
+
+		
+}
---
+++ NetbeansProjects/BootCamp/src/bootcamp/Main.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bootcamp;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author f3nix
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("* Bootcamp date availability check *");
+		System.out.println();
+		System.out.println("Enter 1st Bootcamp details");
+		BootCamp bc1 = createBC(sc);
+		System.out.println();
+		System.out.println("Enter 2nd Bootcamp details");
+		BootCamp bc2 = createBC(sc);
+		System.out.println();
+		System.out.println("Enter 3rd Bootcamp details");
+		BootCamp bc3 = createBC(sc);
+
+		List<BootCamp> bcList = new ArrayList<>();
+		bcList.add(bc1);
+		bcList.add(bc2);
+		bcList.add(bc3);
+
+		checkDate(sc, bcList);
+
+	}
+
+	public static String inputString(Scanner sc, String message) {
+		System.out.print(message);
+		return sc.next();
+	}
+
+	public static LocalDate inputDate(Scanner sc, String message) {
+		LocalDate date;
+		while (true) {
+			System.out.print(message);
+			String temp = sc.next();
+			try {
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+				date = LocalDate.parse(temp, formatter);
+				break;
+			} catch (DateTimeParseException e) {
+				System.out.println("Invalid Date.");
+			}
+		}
+		return date;
+	}
+
+	public static BootCamp createBC(Scanner sc) {
+		BootCamp bootcamp = new BootCamp(inputString(sc, "Enter title: "),
+				inputDate(sc, "Enter start date (dd/mm/yyyy): "),
+				inputDate(sc, "Enter end date (dd/mm/yyyy): "));
+		return bootcamp;
+	}
+
+	public static void checkDate(Scanner sc, List<BootCamp> bc) {
+		System.out.println();
+		LocalDate date = inputDate(sc, "Check available date: ");
+		List<BootCamp> bcCandidates = new ArrayList<>();
+		for (BootCamp bootcamp : bc) {
+			if (!(date.isBefore(bootcamp.getStartingDate()) || date.isAfter(bootcamp.getEndingDate()))) {
+				bcCandidates.add(bootcamp);
+			}
+		}
+		if (!bcCandidates.isEmpty()) {
+			for (BootCamp bootcamp : bcCandidates) {
+				System.out.println("Bootcamp " + bootcamp.getName()
+						+ " will be available in " + date + ". "
+						+ "It starts " + bootcamp.getStartingDate()
+						+ " and ends " + bootcamp.getEndingDate());
+			}
+			System.out.println();
+		} else {
+			System.out.println("Sorry no bootcamp is available in " + date);
+			System.out.println();
+		}
+	}
+}
