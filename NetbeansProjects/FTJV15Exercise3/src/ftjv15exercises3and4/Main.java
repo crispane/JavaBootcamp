ad51248efd7b894c4b1e3ca57019116f460abf85
crispane <crispane@pm.me>
2020-07-24 02:08:36 +0300

    Add Exercise 4

--- NetbeansProjects/FTJV15Exercise3/src/ftjv15exercises3and4/Main.java
+++ NetbeansProjects/FTJV15Exercise3/src/ftjv15exercises3and4/Main.java
@@ -6,8 +6,8 @@
 *
 * Allow the user to type a word and report how many times that word appeared in the book.
 * Report all words that appeared in the book at least 500 times, in alphabetical order.
-*/
-package ftjv15exercise3;
+ */
+package ftjv15exercises3and4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,10 +22,16 @@ import java.util.TreeMap;
 public class Main {
 
 	public static void main(String[] args) {
+		//Exercise 3
 		Map<String, Integer> journeyMap = wordMap("aJourneyToTheInteriorOfTheEarth.txt");
+		System.out.println(" *** Word map of " + "\"aJourneyToTheInteriorOfTheEarth.txt\" ***");
+		System.out.println();
 		System.out.println(journeyMap);
 		wordFinder(journeyMap);
 		wordOccurrences(journeyMap, 500);
+
+		// Exercise 4
+		portionOfTree(journeyMap);
 	}
 
 	public static Map<String, Integer> wordMap(String directory) {
@@ -34,7 +40,6 @@ public class Main {
 			File text = new File(directory);
 			Scanner sc = new Scanner(text);
 			String word;
-			int count = 0;
 			while (sc.hasNext()) {
 				word = sc.next();
 				if (map.containsKey(word)) {
@@ -74,6 +79,23 @@ public class Main {
 	public static String inputString(Scanner sc, String message) {
 		System.out.print(message);
 		return sc.next();
+	}
+
+	public static Map<String, Integer> portionOfTree(Map<String, Integer> map) {
+		System.out.println();
+		System.out.print("Enter a letter to map words after it: ");
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		Map<String, Integer> portion = new TreeMap();
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if (entry.getKey().charAt(0) >= word.charAt(0)) {
+				portion.put(entry.getKey(), entry.getValue());
+			}
+		}
+		System.out.println();
+		System.out.println("The portion of the map after \"" + word.charAt(0) + "\" is: ");
+		System.out.println(portion);
+		return portion;
 	}
 
 }
