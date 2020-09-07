f7f5a432771c5827489a32813c6d0146fecf8b56
Cris Pane <crispane@pm.me>
2020-09-07 13:34:17 +0300

    fix formatting

--- mySqlProjects/BCbase.sql
+++ mySqlProjects/BCbase.sql
@@ -5,56 +5,56 @@ use bootcamps;
 CREATE TABLE classes (
 	class_id INT AUTO_INCREMENT PRIMARY KEY,
 	class_title VARCHAR(50),
-    class_stream VARCHAR(50),
-    class_type VARCHAR(50),
-    start_date DATE,
-    end_date DATE
+	class_stream VARCHAR(50),
+	class_type VARCHAR(50),
+	start_date DATE,
+	end_date DATE
 );
 
 CREATE TABLE assignments (
 	assignment_id INT AUTO_INCREMENT PRIMARY KEY,
 	assignment_title VARCHAR(50),
-    assignment_description VARCHAR(100),
-    submission_date DATE,
-    class_id INT,
-    FOREIGN KEY (class_id) REFERENCES classes (class_id)
+	assignment_description VARCHAR(100),
+	submission_date DATE,
+	class_id INT,
+	FOREIGN KEY (class_id) REFERENCES classes (class_id)
 );
 
 CREATE TABLE projects (
 	project_id INT AUTO_INCREMENT PRIMARY KEY,
 	project_title VARCHAR(50),
-    project_description VARCHAR(100),
-    class_id INT,
+	project_description VARCHAR(100),
+	class_id INT,
 	FOREIGN KEY (class_id) REFERENCES classes (class_id)
 );
 
 CREATE TABLE students (
 	student_id INT AUTO_INCREMENT PRIMARY KEY,
-    student_name VARCHAR(50),
-    student_dob DATE,
-    student_fees INT,
-    class_id INT,
-	CONSTRAINT FK_student_class FOREIGN KEY (class_id) REFERENCES classes (class_id)
+	student_name VARCHAR(50),
+	student_dob DATE,
+	student_fees INT,
+	class_id INT,
+	FOREIGN KEY (class_id) REFERENCES classes (class_id)
 );
 
 CREATE TABLE assignments_results (
 	-- assm_res_id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id INT,
+	student_id INT,
 	class_id INT,
 	assignment_id INT,
 	mark INT,
-    FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (class_id) REFERENCES students(class_id),
-    FOREIGN KEY (assignment_id) REFERENCES assignments(assignment_id)
+	FOREIGN KEY (student_id) REFERENCES students(student_id),
+	FOREIGN KEY (class_id) REFERENCES students(class_id),
+	FOREIGN KEY (assignment_id) REFERENCES assignments(assignment_id)
 );
 
 CREATE TABLE projects_results (
-	project_res_id INT AUTO_INCREMENT PRIMARY KEY,
-    mark INT,
-    student_id INT,
+	-- project_res_id INT AUTO_INCREMENT PRIMARY KEY,
+	mark INT,
+	student_id INT,
 	class_id INT,
 	project_id INT,
-    FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (class_id) REFERENCES classes(class_id),
-    FOREIGN KEY (project_id) REFERENCES projects(project_id)
+	FOREIGN KEY (student_id) REFERENCES students(student_id),
+	FOREIGN KEY (class_id) REFERENCES classes(class_id),
+	FOREIGN KEY (project_id) REFERENCES projects(project_id)
 );
